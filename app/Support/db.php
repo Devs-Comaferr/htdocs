7ece1c575702a8e3426c6df8416f75aa20c4b0cb
Dep. Informatica <informatica@comaferr.es>
2026-03-29 19:11:23 +0200

    fix: ajustar conexión ODBC a connection string directa desde runtime_secrets

--- app/Support/db.php
+++ app/Support/db.php
@@ -38,7 +38,7 @@ if (!function_exists('openOdbcConnection')) {
             || empty($config['password'])
         ) {
             error_log('ODBC connection config incomplete. Define DB_HOST/DB_NAME/DB_USER/DB_PASS o DB_DSN/DB_USER/DB_PASS en entorno o config local.');
-            throw new Exception('Error de conexión a BD');
+            throw new Exception('Error de conexión a BD: ' . print_r($config, true));
         }
 
         $connection = @odbc_connect($config['dsn'], $config['username'], $config['password']);
--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once dirname(__DIR__) . '/bootstrap/init.php';
 require_once BASE_PATH . '/app/Modules/Home/index.php';
