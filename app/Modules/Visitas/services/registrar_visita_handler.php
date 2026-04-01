5df150360b57c6868a6f8aafecb3e912b9865298
Dep. Informatica <informatica@comaferr.es>
2026-04-01 17:43:17 +0200

    Visitas cerrado - antes de refactor Planificador

--- app/Modules/Visitas/services/registrar_visita_handler.php
+++ app/Modules/Visitas/services/registrar_visita_handler.php
@@ -5,4 +5,5 @@ require_once BASE_PATH . '/app/Support/functions.php';
 require_once BASE_PATH . '/app/Modules/Visitas/services/VisitasValidationService.php';
 require_once BASE_PATH . '/app/Modules/Visitas/services/VisitasAjaxService.php';
 require_once BASE_PATH . '/app/Modules/Visitas/services/VisitasQueryService.php';
+require_once BASE_PATH . '/app/Modules/Visitas/services/VisitasCalendarioService.php';
 require_once BASE_PATH . '/app/Modules/Visitas/services/VisitasService.php';
